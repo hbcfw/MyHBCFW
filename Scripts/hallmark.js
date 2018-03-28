e5b34cee327fea35e8a3fd485a5abdfcca1b613b
Hallmark Baptist Church <nathan@hbcfw.org>
2018-03-28 16:13:35 -0500

    JS Fix
    Added missing #

--- Scripts/hallmark.js
+++ Scripts/hallmark.js
@@ -3,5 +3,5 @@ jQuery(function(){
     var images = ['https://hbcfw.org/wp-content/uploads/2017/03/cropped-ft-worth.jpg', 'https://hbcfw.org/wp-content/uploads/2017/03/banner_home-v2.jpg', 'https://hbcfw.org/wp-content/uploads/2017/03/banner_greeters.jpg'];
         
     //Build the img, then do a bit of maths to randomize load and append to a div. Add a touch off css to fade them badboys in all sexy like.
-    $( "banner-bg" ).attr( 'src', images[Math.floor(Math.random() * images.length)] );
+    $( "#banner-bg" ).attr( 'src', images[Math.floor(Math.random() * images.length)] );
 });
